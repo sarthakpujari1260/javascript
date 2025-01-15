0d6fb668c2214729b2886aea31d7e000cbaf7e02
sarthak pujari <sarthakpujari1260@gmail.com>
2025-01-16 00:07:59 +0530

    js

---
+++ 23_Iterations.js
@@ -0,0 +1,16 @@
+// FOR LOOP 
+
+// for (let i=1 ; i<=10 ; i++) {
+//     const element = i
+//     console.log(element);
+    
+    
+// }
+
+for(let i =0 ; i<=5 ; i++){
+    for(let j=i ; j=i+1 ;j++)
+    {
+        console.log("*");
+        
+    }
+}
