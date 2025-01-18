f6b0bd2143db4e5e76d069237cda2468d838151c
sarthak pujari <sarthakpujari1260@gmail.com>
2025-01-18 15:01:50 +0530

    js

--- 23_Iterations.js
+++ 23_Iterations.js
@@ -7,10 +7,36 @@
     
 // }
 
-for(let i =0 ; i<=5 ; i++){
-    for(let j=i ; j=i+1 ;j++)
-    {
-        console.log("*");
+// for(let i =0 ; i<=5 ; i++){
+//     for(let j=i ; j=i+1 ;j++)
+//     {
+//         //console.log("*");
         
+//     }
+// }
+
+let myArray = ["Superman" , "Batman" , "Flash"]
+console.log(myArray.length);
+
+
+for (let i = 0; i <= myArray.length; i++) {
+    const Superheroes = myArray[i]
+    
+ //console.log(Superheroes);
+    
+}
+
+
+// break and continue 
+
+for (let index = 1; index <=20; index++) {
+
+    if (index == 5) {
+    console.log("5 is the best");
+    //break
+    //continue
     }
+   console.log(`Value of i is ${index}`);
+   
+    
 }
