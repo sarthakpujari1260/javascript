44c1dd86642cd2757ab14e66d443567d95b45999
sarthak pujari <sarthakpujari1260@gmail.com>
2025-01-18 15:45:14 +0530

    js

---
+++ 24_While&DoWhileLoops.js
@@ -0,0 +1,27 @@
+
+// WHILE LOOP
+
+let index = 0
+while (index<=10) {
+    //console.log(index);
+    index = index+2
+}
+
+
+let myArray = ["Flash","Superman","Spiderman"]
+
+let number = 0 
+while (number <myArray.length) {
+    //console.log(`The value is ${myArray[number]}`);
+    number++  
+}
+
+
+// DO WHILE LOOP 
+
+let score = 1
+do {
+    console.log(score);
+    score++
+    
+} while (score<=10);
