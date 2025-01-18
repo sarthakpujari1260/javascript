7bd4347ef7bdd490e0249231c911d5e2e87710d2
sarthak pujari <sarthakpujari1260@gmail.com>
2025-01-18 20:41:57 +0530

    js

---
+++ 25_HighOrderArrayLoops.js
@@ -0,0 +1,39 @@
+// ARRAYS specific Loops [{} , {}, {}]
+
+
+// FOR OF 
+
+const arr = [1,2,3,4,5]
+
+for (const i of arr) {
+    //console.log(i);
+    
+}
+
+const greetings = "hello world !"
+for (const i of greetings) {
+    //console.log(i);
+    
+    
+}
+
+
+
+// MAPS => they basically holds the unique values. Means repeated statements are written only once and are uniquely identified.
+
+const maps = new Map ()
+ 
+maps.set("GAME 1" , "GTA")
+maps.set("GAME 2" , "NFS")
+maps.set("GAME 3" , "VALO")
+maps.set("GAME 4" , "GOW")
+maps.set("GAME 1" , "GTA")
+
+
+for (const [key , value] of maps) {
+    console.log(`${key} = ${value}`);
+    
+    
+}
+
+
